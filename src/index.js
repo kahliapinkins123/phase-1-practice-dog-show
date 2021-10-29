66b77ba36963478ac5638993301be3908ce1029a
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-29 16:10:47 -0400

    commit tree 1b90ba0f25fe427c9c3058a7aa171875cfb1be1b

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(resp => resp.json())
             .then(obj => {
                 createTable(obj);
-                console.log(obj)
             })
     }
 
@@ -73,77 +72,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 'sex': inputSex.value
             })
 
-        })//.then(resp=>resp.json()).then(obj=>console.log(obj))
-        fetchDogs();
-        console.log(dog);
-
+        }).then(resp => resp.json()).then(obj => {
+            tableBody.innerHTML = '';
+            fetchDogs();
+            inputName.value = '';
+            inputBreed.value = '';
+            inputSex.value = '';
+        })
     }
-
     fetchDogs();
 
 })
-
-
-/*    function fetchDogs(){
-        fetch('http://localhost:3000/dogs')
-        .then(resp=>resp.json())
-        .then(obj=>{
-            for(const dog of obj){
-                const tr = document.createElement('tr');
-                const nameTd = document.createElement('td');
-                const breedTd = document.createElement('td');
-                const sexTd = document.createElement('td');
-                const btnTd = document.createElement('td');
-                const btn = document.createElement('BUTTON');
-
-                nameTd.textContent = dog['name'];
-                breedTd.textContent = dog['breed'];
-                sexTd.textContent = dog['sex'];
-                btn.textContent = 'Edit Dog';
-
-                btnTd.appendChild(btn);
-                tr.appendChild(nameTd);
-                tr.appendChild(breedTd);
-                tr.appendChild(sexTd);
-                tr.appendChild(btnTd);
-
-                tableBody.appendChild(tr);
-
-                btn.addEventListener('click', ()=>{
-                    inputName.value = dog['name'];
-                    inputBreed.value = dog['breed'];
-                    inputSex.value = dog['sex'];
-
-                    form.addEventListener('submit',e=>{
-                        e.preventDefault();
-
-                        fetch(`http://localhost:3000/dogs/${dog['id']}`,{
-                            method: 'PATCH',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                Accept: 'application/json'
-                            },
-                            body: JSON.stringify({
-                                'name': inputName.value,
-                                'breed': inputBreed.value,
-                                'sex': inputSex.value
-                            })
-                        })
-                        nameTd.textContent = inputName.value;
-                        breedTd.textContent = inputBreed.value;
-                        sexTd.textContent = inputSex.value;
-
-                    })
-                });
-
-            }
-        })
-    }*/
-
-//   fetchDogs();
-
-//   form.addEventListener('submit',e=>{
-//       e.preventDefault();
- //  })
-
-//})
