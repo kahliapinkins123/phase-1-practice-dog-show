563c450a794739176b0f606114726172378ef118
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-29 15:34:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
-   const tableBody = document.querySelector('#table-body');
-   const form = document.querySelector('form');
-   const inputName = document.querySelector('[name="name"]')
-   const inputBreed = document.querySelector('[name="breed"]')
-   const inputSex = document.querySelector('[name="sex"]')
+    const tableBody = document.querySelector('#table-body');
+    let form = document.querySelector('form');
+    const inputName = document.querySelector('[name="name"]')
+    const inputBreed = document.querySelector('[name="breed"]')
+    const inputSex = document.querySelector('[name="sex"]')
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const dog = {
+            name: inputName.value,
+            breed: inputBreed.value,
+            sex: inputSex.value,
+            id: form.dataset.id
+        }
+        updateDog(dog);
+    })
+
+    function fetchDogs() {
+        fetch('http://localhost:3000/dogs')
+            .then(resp => resp.json())
+            .then(obj => {
+                createTable(obj);
+                console.log(obj)
+            })
+    }
+
+    function createTable(obj) {
+        for (const dog of obj) {
+            const tr = document.createElement('tr');
+            const nameTd = document.createElement('td');
+            const breedTd = document.createElement('td');
+            const sexTd = document.createElement('td');
+            const btnTd = document.createElement('td');
+            const btn = document.createElement('BUTTON');
+
+            nameTd.textContent = dog['name'];
+            breedTd.textContent = dog['breed'];
+            sexTd.textContent = dog['sex'];
+            btn.textContent = 'Edit Dog';
+
+            btnTd.appendChild(btn);
+            tr.appendChild(nameTd);
+            tr.appendChild(breedTd);
+            tr.appendChild(sexTd);
+            tr.appendChild(btnTd);
+
+            tableBody.appendChild(tr);
+
+            btn.addEventListener('click', () => {
+                fillForm(dog);
+            })
+
+        }
+    }
+    function fillForm(dog) {
+        inputName.value = dog['name'];
+        inputBreed.value = dog['breed'];
+        inputSex.value = dog['sex'];
+        form.dataset.id = dog['id'];
+    }
+
+    function updateDog(dog) {
+        fetch(`http://localhost:3000/dogs/${dog['id']}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify({
+                'name': inputName.value,
+                'breed': inputBreed.value,
+                'sex': inputSex.value
+            })
+
+        })//.then(resp=>resp.json()).then(obj=>console.log(obj))
+        fetchDogs();
+        console.log(dog);
+
+    }
+
+    fetchDogs();
+
+})
 
 
-    function fetchDogs(){
+/*    function fetchDogs(){
         fetch('http://localhost:3000/dogs')
         .then(resp=>resp.json())
         .then(obj=>{
@@ -34,11 +112,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 btn.addEventListener('click', ()=>{
                     inputName.value = dog['name'];
                     inputBreed.value = dog['breed'];
-                    inputSex.value = dog['sex'];  
-                    
+                    inputSex.value = dog['sex'];
+
                     form.addEventListener('submit',e=>{
                         e.preventDefault();
-    
+
                         fetch(`http://localhost:3000/dogs/${dog['id']}`,{
                             method: 'PATCH',
                             headers: {
@@ -54,18 +132,18 @@ document.addEventListener('DOMContentLoaded', () => {
                         nameTd.textContent = inputName.value;
                         breedTd.textContent = inputBreed.value;
                         sexTd.textContent = inputSex.value;
-                        
+
                     })
                 });
 
             }
         })
-    }
+    }*/
 
-    fetchDogs();
+//   fetchDogs();
 
-    form.addEventListener('submit',e=>{
-        e.preventDefault();
-    })
+//   form.addEventListener('submit',e=>{
+//       e.preventDefault();
+ //  })
 
-})
+//})
